84d102840b6ddfabf94662def0616f0c0e9c7647
zaaferahhamid2 <zh34@students.uwf.edu>
2025-06-09 21:05:57 -0500

    Update Collatz.java

--- OSProject1/src/collatz/Collatz.java
+++ OSProject1/src/collatz/Collatz.java
@@ -4,6 +4,14 @@ public class Collatz {
 	public static int computeCollatzStoppingTime(int n) {
 		int counter = 0;
 		// Add check for values less than 1, non-whole numbers, and negative values.
+		/* while (n != 1) {
+			if (n & 1 == 0){
+				n = n / 2;
+			}else {
+				n = (3 * n) + 1;
+			}
+   		another way to possibly do if n>1 does not work?
+		*/
 		while (n > 1) {
 			if (n % 2 == 0) {
 				n = n / 2;
